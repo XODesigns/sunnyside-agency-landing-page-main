76a5ce7ff257b22f008d21bcb4c71916fe6cce6d
XODesigns <xolelanimdima@gmail.com>
2022-04-16 13:42:50 +0200

    upload site

--- js/script.js
+++ js/script.js
@@ -2,13 +2,10 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", mobileMenu);
+navMenu.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
 
-if ($('.hamburger').is(':active')) {
-    $(this).addClass("fixed-position");
-    $('.nav-menu').addClass("fixed-position");
- }
